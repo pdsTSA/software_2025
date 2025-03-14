7b1bbd512265da50f98f87f3c0adba096249e00d
jorgevaldezzz <valdezjorge841@gmail.com>
2025-03-13 22:37:08 -0400

    header

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -6,6 +6,7 @@ import {Button} from "@mui/material";
 import {UploadModal} from "~/components/upload-modal";
 import {useState} from "react";
 import {CloudUpload, FileUpload} from "@mui/icons-material";
+import {Typography} from "@mui/material";
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -25,6 +26,7 @@ const SplashScreen = styled('div')`
     > h1 {
         font-size: xxx-large;
         font-weight: bold;
+        color: white;
     }
     
     > p {
@@ -38,6 +40,11 @@ const UploadSection = styled('div')`
     flex-direction: column;
     justify-content: center;
 `
+const TitleText = styled(Typography)({
+    fontWeight: 'bold',
+    color: "white",
+    fontSize: '1.1rem',
+  });
 
 const UploadButton = styled('div')`
     border: 4px solid white;
@@ -61,10 +68,11 @@ export default function Home() {
     return (
         <div>
             <UploadModal open={open} toggleOpen={isOpen} />
-            <CoverImage>
+            <CoverImage style={{ backgroundImage: 'url(https://raw.githubusercontent.com/pdsTSA/software_2025/master/public/creek.jpg)'}}>
                 <SplashScreen>
                     <h1>Clean Up Service</h1>
-                    <p>Saving creeks one picture at a time</p>
+                    <p><TitleText>Saving creeks one picture at a time</TitleText></p>
+                    <p><TitleText>Your images help make the world a cleaner place</TitleText></p>
                 </SplashScreen>
             </CoverImage>
             <ColorBlock color={theme.palette.primary.main}>
