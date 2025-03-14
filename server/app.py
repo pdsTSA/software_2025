2d080195efd884875d0f980e32d70f4431ec89f1
Charlie Boalch <112891042+charlieboalch@users.noreply.github.com>
2025-03-13 23:33:48 -0400

    Update server files

--- server/app.py
+++ server/app.py
@@ -3,21 +3,78 @@ import mimetypes
 import os
 import uuid
 
+import numpy
 import psycopg
 from dotenv import dotenv_values
 from flask import Flask, request, send_file
 from flask_cors import cross_origin, CORS
 from psycopg import ClientCursor
+from sklearn.cluster import AffinityPropagation
+from geopy.geocoders import Nominatim
 
 config = dotenv_values(".env")
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
+geolocator = Nominatim(user_agent="Paul Duke STEM TSA")
+geocache = {}
+
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route('/clusters')
+def get_clusters():
+    with psycopg.connect(f"dbname={config['DATABASE']} "
+                         f"user={config['USERNAME']} "
+                         f"password={config['PASSWORD']}", cursor_factory=ClientCursor) as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT latitude, longitude, "timestamp" FROM reports')
+            raw = cur.fetchall()
+            points = numpy.array(list(map(lambda x: (x[0], x[1]), raw)))
+
+            clf = AffinityPropagation()
+
+            clusters = {}
+            for (point, cluster) in zip(raw, clf.fit_predict(points)):
+                if cluster in clusters:
+                    clusters[int(cluster)].append(point)
+                else:
+                    clusters[int(cluster)] = [point]
+
+            averaged = {}
+            for (cluster, arr) in clusters.items():
+                avg_lat = sum(map(lambda x: x[0], arr)) / len(arr)
+                avg_lng = sum(map(lambda x: x[1], arr)) / len(arr)
+
+                latest = max(map(lambda x: x[2], arr))
+                center = f"{avg_lat}, {avg_lng}"
+
+                if center in geocache:
+                    city_state = geocache[center]
+                else:
+                    location = geolocator.reverse(f"{avg_lat}, {avg_lng}", exactly_one=True)
+                    possible_keys = ['city', 'town']
+                    city = ""
+
+                    for i in possible_keys:
+                        if i in location.raw['address']:
+                            city = location.raw['address'][i]
+
+                    city_state = f"{city}, {location.raw['address']['state']}"
+                    geocache[center] = city_state
+
+                averaged[cluster] = {
+                    "location": city_state,
+                    "center": (avg_lat, avg_lng),
+                    "entries": len(arr),
+                    "latest": latest
+                }
+
+            return averaged
 
 
 @app.route('/image', methods=["GET"])
